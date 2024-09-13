6cdf1b08855a6af00a0ec2e5a3c884d968c6dd6f
Sunjoo Park <all4dich@gmail.com>
2024-09-13 15:09:29 +0900

    Use 'w -h' command to get logged user information
    :Detailed Notes:
      - Get these information
        > user_name, tty, when, from_location

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import (
 	"fmt"
 	"net/http"
 	"os/exec"
+	"strconv"
 	"strings"
 )
+
+// Declare 'port' variable and assign it to 8080
+// Get port nubmer from environment variable
+var port = 18080
 
 func getLoggedInUsers() (string, error) {
 	// Execute the 'who' command to get logged in users
@@ -38,6 +43,19 @@ func metricsHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Prometheus gauge metric format
 	metrics += fmt.Sprintf("logged_in_users %d\n", userCount)
+	// Loop users and print them
+	for _, user := range strings.Split(users, "\n") {
+		if user != "" {
+			user_info := strings.Fields(user)
+			user_name := user_info[0]
+			tty := user_info[1]
+			when := user_info[2]
+			from_location := user_info[4][1:]
+			from_location = from_location[:len(from_location)-1]
+			metrics += fmt.Sprintf("logged_in_user{user=\"%s\", tty=\"%s\", when=\"%s\", from=\"%s\"} 1\n",
+				user_name, tty, when, from_location)
+		}
+	}
 
 	// Write response in Prometheus format
 	w.Header().Set("Content-Type", "text/plain")
@@ -49,8 +67,9 @@ func main() {
 	http.HandleFunc("/metrics", metricsHandler)
 
 	// Start the HTTP server on port 8080
-	fmt.Println("Starting server on :8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	//fmt.Println("Starting server on : %d ...", port)
+	fmt.Printf("Starting server on : %d ...", port)
+	if err := http.ListenAndServe(":"+strconv.Itoa(port), nil); err != nil {
 		fmt.Println("Error starting server:", err)
 	}
 }
