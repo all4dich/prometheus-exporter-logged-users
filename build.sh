98b739c00ad54389e1af55dc63dfcf43704f7b15
Sunjoo Park <all4dich@gmail.com>
2024-09-20 10:27:30 +0900

    Build artifacts for linux/amd64 and darwin/arm64

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 bazel clean
 bazel build //:deploy --platforms=@io_bazel_rules_go//go/toolchain:linux_amd64
 cp bazel-bin/prometheus-exporter-logged-users ./prometheus-exporter-logged-users
+bazel clean
+bazel build //:deploy --platforms=@io_bazel_rules_go//go/toolchain:darwin_arm64
+cp bazel-bin/prometheus-exporter-logged-users ./prometheus-exporter-logged-users-darwin-arm64
