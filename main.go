b3b5468c48156902719c720d79522f4d1a5dec18
Sunjoo Park <all4dich@gmail.com>
2025-01-08 17:07:57 +0900

    common: Create a handler func to send metrics to InfluxDB

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"time"
 )
 
 var port int
@@ -140,6 +141,152 @@ func getProcesses_with_mem_cpu() (string, error) {
 	}
 	processes := strings.TrimSpace(string(out))
 	return processes, nil
+}
+
+func metricsHandler_push() {
+	users, err := getLoggedInUsers()
+	host_name, err := getHostname()
+	my_processes, err_get_process := getProcesses()
+	process_with_mem_cpu, err_get_process_mem_cpu := getProcesses_with_mem_cpu()
+
+	fmt.Println(my_processes)
+	if err != nil {
+		fmt.Println("Error fetching logged-in users:", err)
+		return
+	}
+	if err_get_process != nil {
+		fmt.Println("Error fetching process:", err_get_process)
+		return
+	}
+	if err_get_process_mem_cpu != nil {
+		fmt.Println("Error fetching process with mem and cpu:", err_get_process_mem_cpu)
+		return
+	}
+
+	// Prepare Prometheus format metrics
+	metrics := "# HELP logged_in_users List of currently logged-in users.\n"
+	metrics += "# TYPE logged_in_users gauge\n"
+	metrics += "# HELP process_read_write_in_KB List of currently running processes with Read/Write KB/s.\n"
+	metrics += "# TYPE process_read_write_in_KB gauge\n"
+
+	// Count the number of users (each line in the 'who' output represents one user)
+	userCount := 0
+	if users != "" {
+		userCount = len(strings.Split(users, "\n")) - 2
+	}
+
+	// Prometheus gauge metric format
+	metrics += fmt.Sprintf("logged_in_users{hostname=\"%s\"} %d\n", host_name, userCount)
+	// Loop users and print them
+	// Remove the first two lines of the 'who' output as they are headers
+	for _, user := range strings.Split(users, "\n")[2:] {
+		if user != "" {
+			user_info := strings.Fields(user)
+			user_name := user_info[0]
+			tty := user_info[1]
+			from_location := user_info[2]
+			when := user_info[3]
+			idle_time := user_info[4]
+			jcpu_time := user_info[5]
+			pcpu_time := user_info[6]
+			what_command := user_info[7:]
+			what_command_str := strings.Join(what_command, " ")
+			metrics += fmt.Sprintf("logged_in_user{hostname=\"%s\", user=\"%s\", tty=\"%s\", from=\"%s\", when=\"%s\", idle=\"%s\", jcpu=\"%s\", pcpu=\"%s\", what=\"%s\"} 1\n",
+				host_name, user_name, tty, from_location, when, idle_time, jcpu_time, pcpu_time, what_command_str)
+		}
+	}
+	if my_processes != "" {
+		for _, process := range strings.Split(my_processes, "\n") {
+			if process != "" {
+				// Create code to catch exception
+				process_info := strings.Fields(process)
+				fmt.Println(process_info)
+				process_id := process_info[0]
+				user_name := process_info[2]
+				read_Ks := process_info[3]
+				write_Ks := process_info[5]
+
+				hierarchyId := ""
+				subsystem := ""
+				cgroupPath := ""
+				containerId := ""
+				containerName := ""
+				err := error(nil)
+
+				process_id_str, _ := strconv.Atoi(process_id)
+				if hierarchyId, subsystem, cgroupPath, containerId, containerName, err = checkCgroup(process_id_str); err != nil {
+					fmt.Println("Error:", err)
+				} else {
+					fmt.Printf("Hierarchy ID: %s, Subsystem: %s, Cgroup Path: %s, Container ID: %s, Container Name: %s\n", hierarchyId, subsystem, cgroupPath, containerId, containerName)
+				}
+				if containerId == "" {
+					containerId = "0 N/A"
+					containerName = "0 N/A"
+				}
+
+				if process_info[7] == "?unavailable?" {
+					process_command := process_info[8:]
+					process_command_str := strings.Join(process_command, " ")
+					metrics += fmt.Sprintf("process_read_in_KB{hostname=\"%s\", process_id=\"%s\", username=\"%s\", read=\"%s\", write=\"%s\", container_name=\"%s\", container_id=\"%s\", command=\"%s\"} %s\n",
+						host_name, process_id, user_name, read_Ks, write_Ks, containerId, containerName, process_command_str, read_Ks)
+					metrics += fmt.Sprintf("process_write_in_KB{hostname=\"%s\", process_id=\"%s\", username=\"%s\", read=\"%s\", write=\"%s\", container_name=\"%s\", container_id=\"%s\", command=\"%s\"} %s\n",
+						host_name, process_id, user_name, read_Ks, write_Ks, containerId, containerName, process_command_str, write_Ks)
+				} else {
+					swapin_percent := process_info[7]
+					io_percent := process_info[9]
+					process_command := process_info[11:]
+					process_command_str := strings.Join(process_command, " ")
+					metrics += fmt.Sprintf("process_read_in_KB{hostname=\"%s\", process_id=\"%s\", username=\"%s\", read=\"%s\", write=\"%s\", swapin=\"%s\", io=\"%s\", command=\"%s\"} %s\n",
+						host_name, process_id, user_name, read_Ks, write_Ks, swapin_percent, io_percent, process_command_str, read_Ks)
+					metrics += fmt.Sprintf("process_write_in_KB{hostname=\"%s\", process_id=\"%s\", username=\"%s\", read=\"%s\", write=\"%s\", swapin=\"%s\", io=\"%s\", command=\"%s\"} %s\n",
+						host_name, process_id, user_name, read_Ks, write_Ks, swapin_percent, io_percent, process_command_str, write_Ks)
+				}
+			}
+		}
+	}
+	if process_with_mem_cpu != "" {
+		//fmt.Println("Loop: Getting processes with mem and cpu - start")
+		for _, process := range strings.Split(process_with_mem_cpu, "\n") {
+			if process != "" {
+				process_info := strings.Fields(process)
+				username := process_info[0]
+				process_id := process_info[1]
+				cpu_percent := process_info[2]
+				vsz := process_info[3]
+				rss := process_info[4]
+				process_command := process_info[5:]
+				process_command_str := strings.Join(process_command, " ")
+				hierarchyId := ""
+				subsystem := ""
+				cgroupPath := ""
+				containerId := ""
+				containerName := ""
+				err := error(nil)
+				// drop if process_command_str starts with [ or / or < or >
+				if strings.HasPrefix(process_command_str, "[") || strings.HasPrefix(process_command_str, "/") || strings.HasPrefix(process_command_str, "<") || strings.HasPrefix(process_command_str, ">") {
+					continue
+				}
+				process_id_str, _ := strconv.Atoi(process_id)
+				if hierarchyId, subsystem, cgroupPath, containerId, containerName, err = checkCgroup(process_id_str); err != nil {
+					fmt.Println("Error:", err)
+				} else {
+					fmt.Printf("Hierarchy ID: %s, Subsystem: %s, Cgroup Path: %s, Container ID: %s, Container Name: %s\n", hierarchyId, subsystem, cgroupPath, containerId, containerName)
+				}
+				if containerId == "" {
+					containerId = "0 N/A"
+					containerName = "0 N/A"
+				}
+				metrics += fmt.Sprintf("process_cpu_percent{hostname=\"%s\", username=\"%s\", process_id=\"%s\", cpu_percent=\"%s\", vsz=\"%s\", rss=\"%s\", container_name=\"%s\", container_id=\"%s\", command=\"%s\"} %s\n",
+					host_name, username, process_id, cpu_percent, vsz, rss, containerName, containerId, process_command_str, cpu_percent)
+				metrics += fmt.Sprintf("process_vsz{hostname=\"%s\", username=\"%s\", process_id=\"%s\", cpu_percent=\"%s\", vsz=\"%s\", rss=\"%s\", container_name=\"%s\", container_id=\"%s\", command=\"%s\"} %s\n",
+					host_name, username, process_id, cpu_percent, vsz, rss, containerName, containerId, process_command_str, vsz)
+				metrics += fmt.Sprintf("process_rss{hostname=\"%s\", username=\"%s\", process_id=\"%s\", cpu_percent=\"%s\", vsz=\"%s\", rss=\"%s\", container_name=\"%s\", container_id=\"%s\", command=\"%s\"} %s\n",
+					host_name, username, process_id, cpu_percent, vsz, rss, containerName, containerId, process_command_str, rss)
+			}
+		}
+	}
+	// Print metrics to standard output
+	fmt.Println(metrics)
 }
 
 func metricsHandler(w http.ResponseWriter, r *http.Request) {
@@ -312,6 +459,13 @@ func main() {
 	}
 	port = *portPtr
 	http.HandleFunc("/metrics", metricsHandler)
+
+	ticker := time.NewTicker(5 * time.Second)
+	go func() {
+		for range ticker.C {
+			metricsHandler_push()
+		}
+	}()
 
 	// Start the HTTP server on port $port
 	fmt.Printf("Starting logged users collector server on : %d ...\n", port)
