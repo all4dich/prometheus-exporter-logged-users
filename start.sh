42671f6630645784cde84b92c62b4d4f67a93e9a
Sunjoo Park <all4dich@gmail.com>
2025-01-09 17:53:11 +0900

    update start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,13 @@ PORT=49996
 cd $TARGET_DIR
 export PROCPS_USERLEN=20
 export LC_ALL=C
+export OS=$(uname | tr '[:upper:]' '[:lower:]')
+export ARCH=$(uname -m)
+if [ "$ARCH" == "x86_64" ]; then
+  export ARCH="amd64"
+fi
+if [ "$ARCH" == "aarch64" ]; then
+  export ARCH="arm64"
+fi
+cp prometheus-exporter-logged-users-$OS-$ARCH prometheus-exporter-logged-users
 ./prometheus-exporter-logged-users --port $PORT
